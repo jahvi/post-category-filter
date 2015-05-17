4ab347319b2e68fd6f88e8c8cfd0407897dd020a
Javier Villanueva <javiervd@gmail.com>
2015-05-17 08:46:34 +0100

    Update to WP 4.2

--- class-category-filter.php
+++ class-category-filter.php
@@ -18,7 +18,7 @@ class Post_Category_Filter {
      *
      * @var     string
      */
-    const VERSION = '1.2.2';
+    const VERSION = '1.2.3';
 
     /**
      * Unique identifier for your plugin.
