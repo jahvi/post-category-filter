e80da402c734223c093ccebc036ec248738f3339
Javier Villanueva <javier@medialounge.co.uk>
2016-10-25 17:31:57 +0100

    Bump version

--- class-category-filter.php
+++ class-category-filter.php
@@ -18,7 +18,7 @@ class Post_Category_Filter {
      *
      * @var     string
      */
-    const VERSION = '1.2.7';
+    const VERSION = '1.2.8';
 
     /**
      * Unique identifier for your plugin.
